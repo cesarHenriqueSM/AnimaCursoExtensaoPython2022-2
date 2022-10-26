e10a19c7b2096f8e6be04cd460cacce3e506873c
César Henrique <103151947+cesarHenriqueSM@users.noreply.github.com>
2022-10-26 23:30:11 +0000

    Implementação do input e if

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-# Meu primeiro projeto Python
-print("Hello, world!")
-nome = "César Henrique"
-idade = 19
-print(f"Meu nome é {nome} e eu tenho {idade} anos.")
+nome = input("Digite seu nome: ")
+idade = int(input("Olá, "+nome+", qual a sua idade? "))
+dobro = idade * 2
+print(f"{nome}, o dobro de sua idade é igual a {dobro} anos.")
+if idade>=18:
+  print("Você já é maior de idade! Legal, já pode beber ou dirigir! Mas lembre-se, if (beber & dirigir):  print(Cadeia!)")
